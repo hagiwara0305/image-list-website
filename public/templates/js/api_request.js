82d441670594f84980f03ffe67c8955f643748cc
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-12-03 14:08:46 +0900

    漫画の１枚目を表示するように変更

--- public/templates/js/api_request.js
+++ public/templates/js/api_request.js
@@ -2,38 +2,58 @@ var h = $(window).innerHeight();
 var images_display_counter = 1;
 var double_check_flag = true;
 
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
     var winh = $(window).innerHeight() - 10;
 
     /* 一番下までスクロールした場合imageを追加 */
     if ((winh - h) <= $(window).scrollTop() && double_check_flag) {
         double_check_flag = false;
         $.ajax({
-                type: 'GET',
-                url: 'http://localhost/api/get-images?num=' + images_display_counter,
-                dataType: 'json'
-            })
-            .done(function(data, textStatus, jqXHR) {
+            type: 'GET',
+            url: 'http://localhost/api/get-images?num=' + images_display_counter,
+            dataType: 'json'
+        })
+            .done(function (data, textStatus, jqXHR) {
                 console.log(data);
                 images_display_counter++;
                 double_check_flag = true;
-                data.forEach(function(item) {
-                    $('#content').append(
-                        '<a href="#image-form" class="card_rink" rel="modal:open">' +
-                        '<div class="card transition">' +
-                        '<div class="card_image"' +
-                        'style="background-image: url(' + '/public/' +
-                        item['saving_direcory'] + '/' +
-                        item['illust_name'] + '.jpg' +
-                        ')">' +
-                        '</div>' +
-                        '<h3 class="transition">' +
-                        item['title'] +
-                        '</h3>' +
-                        '<br><small>' + item['user_name'] + '</small>' +
-                        '</div>' +
-                        '</a>'
-                    );
+                data.forEach(function (item) {
+                    if (item['page_count'] > 1) {
+                        $('#content').append(
+                            '<a href="#image-form" class="card_rink" rel="modal:open">' +
+                            '<div class="card transition">' +
+                            '<div class="card_image"' +
+                            'style="background-image: url(' + '/public/' +
+                            item['saving_direcory'] + '/' + item['illust_id'] + '/' +
+                            item['illust_name'] + '_0.jpg' +
+                            ')">' +
+                            '</div>' +
+                            '<h3 class="transition">' +
+                            item['title'] +
+                            '</h3>' +
+                            '<br><small>' + item['user_name'] + '</small>' +
+                            '</div>' +
+                            '</a>'
+                        );
+                    } else {
+                        $('#content').append(
+                            '<a href="#image-form" class="card_rink" rel="modal:open">' +
+                            '<div class="card transition">' +
+                            '<div class="card_image"' +
+                            'style="background-image: url(' + '/public/' +
+                            item['saving_direcory'] + '/' +
+                            item['illust_name'] + '.jpg' +
+                            ')">' +
+                            '</div>' +
+                            '<h3 class="transition">' +
+                            item['title'] +
+                            '</h3>' +
+                            '<br><small>' + item['user_name'] + '</small>' +
+                            '</div>' +
+                            '</a>'
+                        );
+                    }
+
                 });
                 console.log(images_display_counter);
             });
