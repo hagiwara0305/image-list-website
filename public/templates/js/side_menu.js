eebe617a9152b934b2369cbf06bf6c5b8dbe997d
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-12-11 11:53:40 +0900

    api側にソート機能を追加

--- public/templates/js/side_menu.js
+++ public/templates/js/side_menu.js
@@ -1,23 +1,38 @@
 var selected_user = 0;
 var selected_sort = 'create_date';
 
-$('.side_menu_user').click(function(event) {
+$('.side_menu_user').click(function (event) {
     console.log(event['currentTarget']['id']);
     selected_user = event['currentTarget']['id'];
 
-    clear_display()
+    toggle_click();
+    clear_display();
 });
 
-$('.dropdown-item').click(function(event) {
+$('.dropdown-item').click(function (event) {
     console.log(event['currentTarget']['id']);
     selected_sort = event['currentTarget']['id'];
 
     clear_display();
 });
 
+$('.toggle').click(toggle_click);
+
 function clear_display() {
     images_display_counter = 1;
     ouble_check_flag = true;
     $('#content').empty();
     $(window).trigger('scroll');
+}
+
+function toggle_click() {
+    $('.toggle').toggleClass('active');
+
+    if ($('.toggle').hasClass('active')) {
+        $('.nav_menu').addClass('active');　 //クラスを付与
+
+    } else {
+        $('.nav_menu').removeClass('active'); //クラスを外す
+
+    }
 }
