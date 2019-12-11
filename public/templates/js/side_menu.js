102ea5867eb23bc7aedda97ca7c95438256ff900
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-12-11 11:34:23 +0900

    apiに送る値を設定

--- public/templates/js/side_menu.js
+++ public/templates/js/side_menu.js
@@ -1,11 +1,23 @@
 var selected_user = 0;
+var selected_sort = 'create_date';
 
 $('.side_menu_user').click(function(event) {
     console.log(event['currentTarget']['id']);
     selected_user = event['currentTarget']['id'];
 
+    clear_display()
+});
+
+$('.dropdown-item').click(function(event) {
+    console.log(event['currentTarget']['id']);
+    selected_sort = event['currentTarget']['id'];
+
+    clear_display();
+});
+
+function clear_display() {
     images_display_counter = 1;
     ouble_check_flag = true;
     $('#content').empty();
     $(window).trigger('scroll');
-})
+}
